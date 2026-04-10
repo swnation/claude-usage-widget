997f06a8da0427163ce95f034a810fd0e527d180
swnation <swnation@gmail.com>
2026-04-10 22:08:45 +0900

    chore: versionCode 6, versionName 1.3.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.claudeusage.widget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.3"
+        versionCode = 6
+        versionName = "1.3.4"
     }
 
     buildTypes {
