6d236d273b36f8e1bb70f834221eed3ba106d0d0
swnation <swnation@gmail.com>
2026-04-11 02:34:07 +0900

    chore: v1.4.0 - Service WebView 제거, 안정화

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.claudeusage.widget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.3.5"
+        versionCode = 8
+        versionName = "1.4.0"
     }
 
     buildTypes {
